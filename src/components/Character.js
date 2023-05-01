d703709a1e2f7f55e4db45ccbbae039818b3ffb1
Michael Gulotta <gulottam@gmail.com>
2023-05-01 14:51:02 -0400

    completed mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import styled from "styled-components";
+import React from "react";
+
+const WrappedChars = styled.div `
+    width: auto;
+    display: flex;
+    justify-content: space-between;
+    align-content: flex-end;
+    padding: 10px;
+    margin: 15px;
+    border: 2px solid black;
+    background: white;
+`
+
+export default function Character ({character}) {
+    return (
+        <WrappedChars>
+            {character.name}
+            {/* <div>{character.height}</div> */}
+            {/* <div>{character.mass}</div> */}
+            {/* <div>{character.hair_color}</div> */}
+            {/* <div>{character.skin_color}</div> */}
+            {/* <div>{character.eye_color}</div> */}
+            <div>{character.birth_year}</div>
+            {/* <div>{character.gender}</div> */}
+            
+        </WrappedChars>
+
+    )
+}
